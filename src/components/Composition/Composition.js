a4f9da903b6dff9634dcf6e89b1469c178aa5bf8
Ivan Glebov <glebov.vanya8@gmail.com>
2021-08-23 12:11:08 +0300

    Time displaying improvements

--- src/components/Composition/Composition.js
+++ src/components/Composition/Composition.js
@@ -219,9 +219,7 @@ export default withStyles(stylesMaterial)(withTranslation()(connect(
   }
   
   timeToRegular = (seconds) => {
-    const hours = (seconds / 3600).toFixed(0)
-    const minutes = (seconds / 60).toFixed(0)
-    return `${(`0${hours}`).slice(-2)}:${(`0${minutes}`).slice(-2)}:${(`0${seconds}`).slice(-2)}`
+    return new Date(seconds * 1000).toISOString().substr(11, 8)
   }
   
   render() {
