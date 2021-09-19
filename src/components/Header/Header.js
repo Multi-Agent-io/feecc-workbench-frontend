953ad788a478841549c02190c64705435925f992
Ivan Glebov <glebov.vanya8@gmail.com>
2021-09-19 23:42:42 +0300

    Added logic for reading codes from barcode-reader

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,17 +7,29 @@ import MVASLogo from '../../static/imageLeft.png'
 import { push, replace } from "connected-react-router";
 import Stopwatch from "@components/Stopwatch/Stopwatch";
 import PropTypes from "prop-types";
+import { doGetBarcode, doGetUnitBiography, doSetCompositionID } from "@reducers/stagesActions";
+import { setQueryValues } from "@reducers/routerActions";
+import { setIn } from "immutable";
 
 export default withTranslation()(connect(
   (store) => ({
-    composition: store.stages.get('composition').toJS(),
-    location   : store.router.location.pathname,
-    unitID     : store.stages.getIn(['unit', 'unit_internal_id']),
+    composition         : store.stages.get('composition').toJS(),
+    unitBiography       : store.stages.getIn(['unit', 'unit_biography']),
+    location            : store.router.location.pathname,
+    unitID              : store.stages.getIn(['unit', 'unit_internal_id']),
+    unitFromQuery       : new URLSearchParams(store.router.location.search).get('unitid'),
+    router              : store.router,
+    finishedCompositions: store.stages.get('finishedCompositionsIDs')?.toJS()
   }),
   (dispatch) => ({
-    redirectToLogin      : () => dispatch(replace('/')),
-    goToMenu             : () => dispatch(replace({ pathname: '/menu' })),
+    redirectToLogin      : () => dispatch(push('/')),
+    goToMenu             : () => dispatch(push({ pathname: '/menu' })),
     redirectToComposition: () => dispatch(push('/composition')),
+    getBarcode           : (successChecker, errorChecker) => doGetBarcode(dispatch, successChecker, errorChecker),
+    setQuery             : (query, url) => setQueryValues(dispatch, query, url),
+    setCompositionID     : (unitID) => doSetCompositionID(dispatch, unitID),
+    getUnitBiography     : (unitID, successChecker, errorChecker) => doGetUnitBiography(dispatch, unitID, successChecker, errorChecker),
+    
   })
 )(class Header extends React.Component {
   
@@ -31,30 +43,64 @@ export default withTranslation()(connect(
     redirectToComposition: PropTypes.func.isRequired
   }
   
+  state = {
+    timerID: null
+  }
+  
   constructor(props) {
     super(props)
     this.stopwatchRef = React.createRef()
   }
   
   componentDidMount() {
-    if (!this.props.composition.employee_logged_in)
+    if (!this.props.composition.employee_logged_in) {
       this.props.redirectToLogin()
+    }
+    this.setState({
+      timerID: setInterval(() => {
+        this.props.getBarcode(
+          (res) => {
+            if (res && res.buffer !== undefined) {
+              if (this.isNumeric(res.buffer) && res.buffer.length === 13) {
+                if (!this.props.finishedCompositions.includes(res.buffer) && this.props.unitID !== res.buffer) {
+                  if(this.props.unitID !== undefined || this.props.unitID !== '') {
+                    this.props.setCompositionID(res.buffer)
+                    this.props.setQuery({ afterPause: true }, this.props.location)
+                  }
+                }
+              } else {
+                this.props.setQuery({ afterPause: undefined }, this.props.location)
+              }
+            }
+            return true
+          },
+          undefined)
+      }, 1000)
+    })
+  }
+  
+  isNumeric = (str) => {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+      !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
   }
   
   componentDidUpdate(prevProps, prevState, snapshot) {
     // If employee_logged_in status changed ->
     if (this.props.composition.employee_logged_in !== prevProps.composition.employee_logged_in)
-      // If employee_logged_in status is false
-      if (!this.props.composition.employee_logged_in)
+      // If 'employee_logged_in' status is false
+      if (!this.props.composition.employee_logged_in) {
         // Do user logout
         this.props.redirectToLogin()
-    // If backend returned operation_ongoing === true
-    if (this.props?.composition.operation_ongoing)
+      }
+    // If backend returned operation_ongoing === true -> start main stopwatch
+    if (this.props?.composition.operation_ongoing) {
       this.stopwatchRef?.current?.start()
-    else {
+    } else {
+      // If no operation is ongoing - redirect to menu.
       if (this.props.location.split('/')[1] !== 'menu'
         && (this.props.unitID === '' || this.props.unitID === undefined)
-        && (this.props.composition.unit_internal_id === '' || this.props.composition.unit_internal_id === undefined)
+        && (this.props.composition.unit_internal_id === '' || this.props.composition.unit_internal_id === undefined || this.props.composition.unit_internal_id === null)
         && this.props.composition.employee_logged_in) {
         this.props.goToMenu()
       }
