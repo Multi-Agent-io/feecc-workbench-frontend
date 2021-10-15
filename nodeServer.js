80015b5eb23376c15d22e5e6ccefdc0b62c87113
Иван Глебов <71008820+IvanGlebov@users.noreply.github.com>
2021-10-15 10:45:11 +0300

    update nodeServer default port to 3000

--- nodeServer.js
+++ nodeServer.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = process.env.NODE_PORT || 8080;
+const port = process.env.NODE_PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'target')))
 
