171b3ef5bc422af1f36881e463093057ca04e505
Иван Глебов <glebov.vanya8@gmail.com>
2022-07-18 00:37:25 +0400

    Added warning modal for Composition and gatherComponents pages

--- src/components/ReloadButton/ReloadButton.js
+++ src/components/ReloadButton/ReloadButton.js
@@ -1,19 +1,38 @@
 import React from "react";
 import styles from "./ReloadButton.module.css";
 import { LoadingButton } from "@mui/lab";
-import { useTranslation } from "react-i18next";
+import { withTranslation } from "react-i18next";
+import { withContext } from "@reducers/context/withContext";
+import ReloadWarning from "../Modals/ReloadWarning/ReloadWarning";
 
-export default function ReloadButton(props) {
-  const {t} = useTranslation();
-
-  return (
-    <div className={styles.buttonWrapper}>
-      <LoadingButton 
-        variant="outlined"
-        color="secondary"
-        size="large"
-        onClick={() => window.location.href = window.location.href}
-      >{t('ReloadPage')}</LoadingButton>
-    </div>
-  );
+class ReloadButton extends React.Component {
+  render() {
+    return (
+      <div className={styles.buttonWrapper}>
+        <LoadingButton
+          variant="outlined"
+          color="secondary"
+          size="large"
+          onClick={() => {
+            const href = window.location.href;
+            const hrefElementsNum = href.split("/").length;
+            const page = href.split("/")[hrefElementsNum - 1];
+            if (page === "composition" || page === "gatherComponents") {
+              this.props.context.onOpen(
+                <ReloadWarning
+                  reloadAction={() =>
+                    (window.location.href = window.location.href)
+                  }
+                />
+              );
+            }
+          }}
+        >
+          {this.props.t("ReloadPage")}
+        </LoadingButton>
+      </div>
+    );
+  }
 }
+
+export default withContext(withTranslation()(ReloadButton));
